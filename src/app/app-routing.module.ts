d8b89532526c88f39c96543c837ebebd5420b944
abdullahGhoneim <abdullahghoneim890@gmail.com>
2019-02-23 20:18:39 +0200

    site navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
+import { AboutUsComponent } from "./components/about-us/about-us.component";
+import { LastNewsComponent } from "./components/last-news/last-news.component";
+import { ConnectUsComponent } from "./components/connect-us/connect-us.component";
 
-const routes: Routes = [{ path: "", component: HomeComponent }];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "about-us", component: AboutUsComponent },
+  { path: "last-news", component: LastNewsComponent },
+  { path: "connect-us", component: ConnectUsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
-import { UpperbarComponent } from './components/upperbar/upperbar.component';
-import { MainNewComponent } from './components/main-new/main-new.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HomeComponent } from "./components/home/home.component";
+import { UpperbarComponent } from "./components/upperbar/upperbar.component";
+import { MainNewComponent } from "./components/main-new/main-new.component";
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { LastNewsComponent } from './components/last-news/last-news.component';
+import { ConnectUsComponent } from './components/connect-us/connect-us.component';
 
 @NgModule({
   declarations: [
@@ -14,13 +17,13 @@ import { MainNewComponent } from './components/main-new/main-new.component';
     NavbarComponent,
     HomeComponent,
     UpperbarComponent,
-    MainNewComponent
+    MainNewComponent,
+    AboutUsComponent,
+    LastNewsComponent,
+    ConnectUsComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, AngularFirestoreModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
