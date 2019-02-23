9b0a0437d56f6d4e7927f833ddffee8025594609
abdullahGhoneim <abdullahghoneim890@gmail.com>
2019-02-23 19:18:48 +0200

    nav bar and upperbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { UpperbarComponent } from './components/upperbar/upperbar.component';
+import { MainNewComponent } from './components/main-new/main-new.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    UpperbarComponent,
+    MainNewComponent
   ],
   imports: [
     BrowserModule,
